a4d8dd5cf44e59f3a632b161fe91ea4846383099
Adrien NEYRON <adrien.neyron@gmail.com>
2023-09-05 10:50:42 +0200

    fix cloudinary with deployement

--- routes/clothes.js
+++ routes/clothes.js
@@ -2,29 +2,29 @@ var express = require('express');
 var router = express.Router();
 const uniqid = require('uniqid');
 
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
+// const cloudinary = require('cloudinary').v2;
+// const fs = require('fs');
 
 const Clothe = require('../models/clothes');
 
 // Pour ajout de la photo prise à l'écran CreatheClotheE
 // POST avec push en DB + ajout au store
-router.post('/upload', async (req, res)=> {
-    const photoPath = `./tmp/${uniqid()}.jpg`;
-    const resultMove = await req.files.photoFromFront.mv(photoPath);
+// router.post('/upload', async (req, res)=> {
+//     const photoPath = `./tmp/${uniqid()}.jpg`;
+//     const resultMove = await req.files.photoFromFront.mv(photoPath);
    
-    if(!resultMove) {
+//     if(!resultMove) {
       
-        const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+//         const resultCloudinary = await cloudinary.uploader.upload(photoPath);
 
        
-        res.json({ result: true, url: resultCloudinary.secure_url });    }
-        else {
-      res.json({ result: false, error: resultMove });
-    }
-    fs.unlinkSync(photoPath);
+//         res.json({ result: true, url: resultCloudinary.secure_url });    }
+//         else {
+//       res.json({ result: false, error: resultMove });
+//     }
+//     fs.unlinkSync(photoPath);
 
-})
+// })
 
 // Pour la page de finalisation de création du vêtement
 // POST avec push en DB + ajout au store
